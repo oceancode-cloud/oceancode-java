084b73946ad32e8742d81cdc7dc41e5f211f900a
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-09-10 21:59:31 +0800

    fix bug

--- oceancode-common-spring-boot/src/main/java/com/oceancode/cloud/common/mq/local/LocalProducer.java
+++ oceancode-common-spring-boot/src/main/java/com/oceancode/cloud/common/mq/local/LocalProducer.java
@@ -11,18 +11,24 @@ import com.oceancode.cloud.api.mq.Producer;
 import com.oceancode.cloud.api.mq.RecordMeta;
 import com.oceancode.cloud.common.errorcode.CommonErrorCode;
 import com.oceancode.cloud.common.exception.BusinessRuntimeException;
+import com.oceancode.cloud.common.util.SessionUtil;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class LocalProducer implements Producer {
     @Override
     public void send(Message<?> message, Callback callback) {
+        List<Object> sessionValues = SessionUtil.getValues();
         CompletableFuture.runAsync(() -> {
             try {
+                SessionUtil.setValues(sessionValues);
                 GlobalLocalQueueManager.send(message);
                 callback.onSuccess(null);
             } catch (InterruptedException e) {
                 callback.onException(e);
+            } finally {
+                SessionUtil.remove();
             }
         });
 
