5d0a69d31f2448f2f3e3ae1d9be2b4029ac9e940
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-10-10 21:57:23 +0800

    add method

--- oceancode-core/src/main/java/com/oceancode/cloud/api/IncrementIdGenerator.java
+++ oceancode-core/src/main/java/com/oceancode/cloud/api/IncrementIdGenerator.java
@@ -1,5 +1,9 @@
 package com.oceancode.cloud.api;
 
 public interface IncrementIdGenerator {
-    Long nextId(String namespace,Long base);
+    Long nextId(String namespace, Long base);
+
+    default Long nextId() {
+        return nextId(null, null);
+    }
 }
