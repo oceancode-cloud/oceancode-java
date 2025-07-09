8043056e4f4c2a5d799ae1b114d592372d2abe78
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-07-09 20:11:15 +0800

    fix bug

--- oceancode-common-web-spring-boot/src/main/java/com/oceancode/cloud/common/web/service/SessionConfig.java
+++ oceancode-common-web-spring-boot/src/main/java/com/oceancode/cloud/common/web/service/SessionConfig.java
@@ -4,6 +4,7 @@ import com.oceancode.cloud.api.cache.LocalCacheService;
 import com.oceancode.cloud.api.cache.RedisCacheService;
 import com.oceancode.cloud.api.session.SessionService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -23,9 +24,9 @@ public class SessionConfig {
     }
 
     @Bean
-    @ConditionalOnMissingBean(SessionService.class)
     @ConditionalOnBean({LocalCacheService.class})
-    @ConditionalOnExpression(value = "'${oc.session.cache.type}'=='caffeine'")
+    @ConditionalOnClass(name = "com.github.benmanes.caffeine.cache.Caffeine")
+    @ConditionalOnProperty(name = "oc.session.cache.type",havingValue = "caffeine", matchIfMissing = true)
     public CaffeineSessionServiceImpl caffeineSessionService() {
         return new CaffeineSessionServiceImpl();
     }
