3c3d562c31a30132d64712186b0666a00ea7be61
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-08-01 22:22:23 +0800

    增加Domain标记

--- oceancode-common-spring-boot/src/main/java/com/oceancode/cloud/common/config/SerializerConfig.java
+++ oceancode-common-spring-boot/src/main/java/com/oceancode/cloud/common/config/SerializerConfig.java
@@ -1,10 +1,13 @@
 package com.oceancode.cloud.common.config;
 
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.oceancode.cloud.api.mq.MessageType;
 import com.oceancode.cloud.api.session.UserType;
 import com.oceancode.cloud.chart.ChartMessageType;
 import com.oceancode.cloud.common.util.JsonUtil;
 import jakarta.annotation.PostConstruct;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -15,5 +18,12 @@ public class SerializerConfig {
         JsonUtil.registerTypeEnum(UserType.class);
         JsonUtil.registerTypeEnum(MessageType.class);
         JsonUtil.registerTypeEnum(ChartMessageType.class);
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> {
+            builder.serializerByType(Long.class, ToStringSerializer.instance);
+        };
     }
 }
