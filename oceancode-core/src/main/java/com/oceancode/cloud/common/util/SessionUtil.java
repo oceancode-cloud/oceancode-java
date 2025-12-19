05e7e93caf8ccf838abb5c03e74ada7802966b8a
1354174190qjw@sina.com <1354174190qjw@sina.com>
2025-12-19 19:56:29 +0800

    fix bug

--- oceancode-core/src/main/java/com/oceancode/cloud/common/util/SessionUtil.java
+++ oceancode-core/src/main/java/com/oceancode/cloud/common/util/SessionUtil.java
@@ -61,10 +61,7 @@ public final class SessionUtil {
 
     public static void setRequestId(String id) {
         if (ValueUtil.isNotEmpty(requestId())) {
-            if (ValueUtil.isEmpty(id)) {
-                return;
-            }
-            throw new BusinessRuntimeException(CommonErrorCode.SERVER_ERROR, "request already initial");
+            return;
         }
         REQUEST_ID.set(id);
     }
