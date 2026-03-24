67eb2f76b4e955dde8153a7c70de63158bf6b6d4
1354174190qjw@sina.com <1354174190qjw@sina.com>
2026-03-24 22:46:33 +0800

    新增模型类

--- oceancode-core/src/main/java/com/oceancode/cloud/plugin/api/object/GGeneratorContext.java
+++ oceancode-core/src/main/java/com/oceancode/cloud/plugin/api/object/GGeneratorContext.java
@@ -1,4 +1,7 @@
 package com.oceancode.cloud.plugin.api.object;
 
+import com.oceancode.cloud.plugin.api.object.model.GModelField;
+
 public interface GGeneratorContext extends GContext {
+    String getFieldType(GModelField<?> field, boolean isList);
 }
